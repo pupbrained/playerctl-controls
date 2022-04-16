1b8f914866fcba1dc8d510e8cde4b56dac286143
marsupialgutz <mars@possums.xyz>
2022-04-16 01:03:48 -0400

    Changed `/playerctl` to `/pctl`

--- src/main/java/net/marsupialgutz/pctl_meta/PlayerctlControls.java
+++ src/main/java/net/marsupialgutz/pctl_meta/PlayerctlControls.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -23,7 +22,7 @@ public class PlayerctlControls implements ModInitializer {
         final var rt = Runtime.getRuntime();
         assert mc.player != null;
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
-            dispatcher.register(literal("playerctl").then(literal("play").executes(context -> {
+            dispatcher.register(literal("pctl").then(literal("play").executes(context -> {
                 try {
                     if (playing) {
                         mc.player.sendMessage(Text.of("Already playing!"), true);
@@ -37,7 +36,7 @@ public class PlayerctlControls implements ModInitializer {
                 playing = true;
                 return 1;
             })));
-            dispatcher.register(literal("playerctl").then(literal("pause").executes(context -> {
+            dispatcher.register(literal("pctl").then(literal("pause").executes(context -> {
                 try {
                     if (!playing) {
                         mc.player.sendMessage(Text.of("Already paused!"), true);
@@ -51,35 +50,32 @@ public class PlayerctlControls implements ModInitializer {
                 }
                 return -1;
             })));
-            dispatcher.register(literal("playerctl").then(literal("playpause").executes(context -> {
+            dispatcher.register(literal("pctl").then(literal("playpause").executes(context -> {
                 try {
-                    final String[] cmd = {"playerctl", "play-pause"};
-                    rt.exec(cmd);
+                    rt.exec("playerctl play-pause");
                     playing = !playing;
                     return 1;
                 } catch (final IOException e) {
                     throw new RuntimeException(e);
                 }
             })));
-            dispatcher.register(literal("playerctl").then(literal("next").executes(context -> {
+            dispatcher.register(literal("pctl").then(literal("next").executes(context -> {
                 try {
-                    final String[] cmd = {"playerctl", "next"};
-                    rt.exec(cmd);
+                    rt.exec("playerctl next");
                     return 1;
                 } catch (final IOException e) {
                     throw new RuntimeException(e);
                 }
             })));
-            dispatcher.register(literal("playerctl").then(literal("previous").executes(context -> {
+            dispatcher.register(literal("pctl").then(literal("previous").executes(context -> {
                 try {
-                    final String[] cmd = {"playerctl", "previous"};
-                    rt.exec(cmd);
+                    rt.exec("playerctl previous");
                     return 1;
                 } catch (final IOException e) {
                     throw new RuntimeException(e);
                 }
             })));
-            dispatcher.register(literal("playerctl").then(literal("togglestatus").executes(context -> {
+            dispatcher.register(literal("pctl").then(literal("togglestatus").executes(context -> {
                 showStatus = !showStatus;
                 return 1;
             })));
