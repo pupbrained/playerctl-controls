49dc71d06b083ed005ddbeb8d2fa5605ff73fcd2
marsupialgutz <mars@possums.xyz>
2022-04-15 20:04:40 -0400

    Refactored and formatted some stuff

--- src/main/java/net/marsupialgutz/pctl_meta/PlayerctlControls.java
+++ src/main/java/net/marsupialgutz/pctl_meta/PlayerctlControls.java
@@ -1,9 +1,6 @@
 package net.marsupialgutz.pctl_meta;
 
-import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.text.Text;
+import static net.minecraft.server.command.CommandManager.literal;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,7 +8,10 @@ import java.io.InputStreamReader;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import static net.minecraft.server.command.CommandManager.literal;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
 
 public class PlayerctlControls implements ModInitializer {
     public static String result;
@@ -19,8 +19,8 @@ public class PlayerctlControls implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        var mc = MinecraftClient.getInstance();
-        var rt = Runtime.getRuntime();
+        final var mc = MinecraftClient.getInstance();
+        final var rt = Runtime.getRuntime();
         assert mc.player != null;
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
             dispatcher.register(literal("playerctl").then(literal("play").executes(context -> {
@@ -29,9 +29,9 @@ public class PlayerctlControls implements ModInitializer {
                         mc.player.sendMessage(Text.of("Already playing!"), true);
                         return 1;
                     }
-                    String[] cmd = {"playerctl", "play"};
+                    final String[] cmd = { "playerctl", "play" };
                     rt.exec(cmd);
-                } catch (IOException e) {
+                } catch (final IOException e) {
                     throw new RuntimeException(e);
                 }
                 playing = true;
@@ -43,39 +43,39 @@ public class PlayerctlControls implements ModInitializer {
                         mc.player.sendMessage(Text.of("Already paused!"), true);
                         return 1;
                     }
-                    String[] cmd = {"playerctl", "pause"};
+                    final String[] cmd = { "playerctl", "pause" };
                     rt.exec(cmd);
                     playing = false;
-                } catch (IOException e) {
+                } catch (final IOException e) {
                     throw new RuntimeException(e);
                 }
                 return -1;
             })));
             dispatcher.register(literal("playerctl").then(literal("playpause").executes(context -> {
                 try {
-                    String[] cmd = {"playerctl", "play-pause"};
+                    final String[] cmd = { "playerctl", "play-pause" };
                     rt.exec(cmd);
                     playing = !playing;
                     return 1;
-                } catch (IOException e) {
+                } catch (final IOException e) {
                     throw new RuntimeException(e);
                 }
             })));
             dispatcher.register(literal("playerctl").then(literal("next").executes(context -> {
                 try {
-                    String[] cmd = {"playerctl", "next"};
+                    final String[] cmd = { "playerctl", "next" };
                     rt.exec(cmd);
                     return 1;
-                } catch (IOException e) {
+                } catch (final IOException e) {
                     throw new RuntimeException(e);
                 }
             })));
             dispatcher.register(literal("playerctl").then(literal("previous").executes(context -> {
                 try {
-                    String[] cmd = {"playerctl", "previous"};
+                    final String[] cmd = { "playerctl", "previous" };
                     rt.exec(cmd);
                     return 1;
-                } catch (IOException e) {
+                } catch (final IOException e) {
                     throw new RuntimeException(e);
                 }
             })));
@@ -85,25 +85,25 @@ public class PlayerctlControls implements ModInitializer {
             })));
         });
         if (showStatus) {
-            var timer = new Timer();
+            final var timer = new Timer();
             timer.schedule(new TimerTask() {
                 @Override
                 public void run() {
                     try {
-                        String[] cmd = {"playerctl", "metadata", "-f", "'{{ artist }} - {{ title }}'"};
+                        String[] cmd = { "playerctl", "metadata", "-f", "'{{ artist }} - {{ title }}'" };
                         Process proc = rt.exec(cmd);
                         BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
                         String s;
                         while ((s = stdInput.readLine()) != null) {
                             result = s;
                         }
-                        cmd = new String[]{"playerctl", "status"};
+                        cmd = new String[] { "playerctl", "status" };
                         proc = rt.exec(cmd);
                         stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
                         while ((s = stdInput.readLine()) != null) {
                             playing = s.equals("Playing");
                         }
-                    } catch (IOException e) {
+                    } catch (final IOException e) {
                         throw new RuntimeException(e);
                     }
                 }
--- src/main/java/net/marsupialgutz/pctl_meta/mixin/InGameHudMixin.java
+++ src/main/java/net/marsupialgutz/pctl_meta/mixin/InGameHudMixin.java
@@ -1,29 +1,36 @@
 package net.marsupialgutz.pctl_meta.mixin;
 
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.util.math.MatrixStack;
+import static net.marsupialgutz.pctl_meta.PlayerctlControls.playing;
+import static net.marsupialgutz.pctl_meta.PlayerctlControls.result;
+import static net.marsupialgutz.pctl_meta.PlayerctlControls.showStatus;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static net.marsupialgutz.pctl_meta.PlayerctlControls.*;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.client.util.math.MatrixStack;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
 
     @Inject(method = "render", at = @At("RETURN"))
 
-    public void onRender(MatrixStack matrices, float tickDelta, CallbackInfo info) {
+    public void onRender(final MatrixStack matrices, final float tickDelta, final CallbackInfo info) {
+        final var mc = MinecraftClient.getInstance();
         if (showStatus && !MinecraftClient.getInstance().options.debugEnabled) {
-            MinecraftClient.getInstance().textRenderer.drawWithShadow(matrices, "Song:", 5, 5, 0xffffff);
+            mc.textRenderer.drawWithShadow(matrices, "Song:", 5, 5, 0xffffff);
             if (playing) {
-                MinecraftClient.getInstance().textRenderer.drawWithShadow(matrices, result.substring(1, result.length() - 1), 5, 15, 0xffffff);
+                mc.textRenderer.drawWithShadow(matrices,
+                        result.substring(1, result.length() - 1), 5, 15, 0xffffff);
             } else {
-                MinecraftClient.getInstance().textRenderer.drawWithShadow(matrices, result.substring(1, result.length() - 1) + " (Paused)", 5, 15, 0xffffff);
+                mc.textRenderer.drawWithShadow(matrices,
+                        result.substring(1, result.length() - 1) + " (Paused)", 5, 15, 0xffffff);
             }
         }
+        mc.close();
     }
 
 }
