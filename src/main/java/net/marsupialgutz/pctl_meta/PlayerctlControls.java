937ac54ef51315e1b961caa3f235d117631a7d4e
marsupialgutz <mars@possums.xyz>
2022-04-16 22:11:27 -0400

    Initial image stuff (kinda fucky still)

--- src/main/java/net/marsupialgutz/pctl_meta/PlayerctlControls.java
+++ src/main/java/net/marsupialgutz/pctl_meta/PlayerctlControls.java
@@ -3,18 +3,25 @@ package net.marsupialgutz.pctl_meta;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.texture.NativeImage;
+import net.minecraft.client.texture.NativeImageBackedTexture;
 import net.minecraft.text.Text;
+
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Objects;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
 public class PlayerctlControls implements ModInitializer {
-    public static String result;
+    public static String result, lastImg;
     public static boolean playing, showStatus = true;
+    public static NativeImageBackedTexture nativeImageBackedTexture;
 
     @Override
     public void onInitialize() {
@@ -23,77 +30,117 @@ public class PlayerctlControls implements ModInitializer {
         assert mc.player != null;
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
             dispatcher.register(literal("pctl").then(literal("play").executes(context -> {
-                try {
-                    if (playing) {
-                        mc.player.sendMessage(Text.of("Already playing!"), true);
+                if (!(result == null)) {
+                    if (!(result.equals("No players found"))) {
+                        try {
+                            if (playing) {
+                                mc.player.sendMessage(Text.of("Already playing!"), true);
+                                return 1;
+                            }
+                            final String[] cmd = {"playerctl", "play"};
+                            rt.exec(cmd);
+                        } catch (final IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        playing = true;
                         return 1;
                     }
-                    final String[] cmd = {"playerctl", "play"};
-                    rt.exec(cmd);
-                } catch (final IOException e) {
-                    throw new RuntimeException(e);
                 }
-                playing = true;
-                return 1;
+                return 0;
             })));
             dispatcher.register(literal("pctl").then(literal("pause").executes(context -> {
-                try {
-                    if (!playing) {
-                        mc.player.sendMessage(Text.of("Already paused!"), true);
-                        return 1;
+                if (!(result == null)) {
+                    if (!(result.equals("No players found"))) {
+                        try {
+                            if (!playing) {
+                                mc.player.sendMessage(Text.of("Already paused!"), true);
+                                return 1;
+                            }
+                            final String[] cmd = {"playerctl", "pause"};
+                            rt.exec(cmd);
+                            playing = false;
+                        } catch (final IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        return -1;
                     }
-                    final String[] cmd = {"playerctl", "pause"};
-                    rt.exec(cmd);
-                    playing = false;
-                } catch (final IOException e) {
-                    throw new RuntimeException(e);
                 }
-                return -1;
+                return 0;
             })));
             dispatcher.register(literal("pctl").then(literal("playpause").executes(context -> {
-                try {
-                    rt.exec("playerctl play-pause");
-                    playing = !playing;
-                    return 1;
-                } catch (final IOException e) {
-                    throw new RuntimeException(e);
+                if (!(result == null)) {
+                    if (!(result.equals("No players found"))) {
+                        try {
+                            rt.exec("playerctl play-pause");
+                            playing = !playing;
+                            return 1;
+                        } catch (final IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
                 }
+                return 0;
             })));
             dispatcher.register(literal("pctl").then(literal("next").executes(context -> {
-                try {
-                    rt.exec("playerctl next");
-                    return 1;
-                } catch (final IOException e) {
-                    throw new RuntimeException(e);
+                if (!(result == null)) {
+                    if (!(result.equals("No players found"))) {
+                        try {
+                            rt.exec("playerctl next");
+                            return 1;
+                        } catch (final IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
                 }
+                return 0;
             })));
             dispatcher.register(literal("pctl").then(literal("previous").executes(context -> {
-                try {
-                    rt.exec("playerctl previous");
-                    return 1;
-                } catch (final IOException e) {
-                    throw new RuntimeException(e);
+                if (!(result == null)) {
+                    if (!(result.equals("No players found"))) {
+                        try {
+                            rt.exec("playerctl previous");
+                            return 1;
+                        } catch (final IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
                 }
+                return 0;
             })));
             dispatcher.register(literal("pctl").then(literal("togglestatus").executes(context -> {
-                showStatus = !showStatus;
-                return 1;
+                if (!(result == null)) {
+                    if (!(result.equals("No players found"))) {
+                        showStatus = !showStatus;
+                        return 1;
+                    }
+                }
+                return 0;
             })));
             dispatcher.register(literal("pctl").then(literal("volup").executes(context -> {
-                try {
-                    rt.exec("playerctl volume 0.1+");
-                    return 1;
-                } catch (final IOException e) {
-                    throw new RuntimeException(e);
+                if (!(result == null)) {
+                    if (!(result.equals("No players found"))) {
+                        try {
+                            rt.exec("playerctl volume 0.1+");
+                            return 1;
+                        } catch (final IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
                 }
+                return 0;
             })));
             dispatcher.register(literal("pctl").then(literal("voldown").executes(context -> {
-                try {
-                    rt.exec("playerctl volume 0.1-");
-                    return 1;
-                } catch (final IOException e) {
-                    throw new RuntimeException(e);
+                if (!(result == null)) {
+                    if (!(result.equals("No players found"))) {
+                        try {
+                            rt.exec("playerctl volume 0.1-");
+                            return 1;
+                        } catch (final IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
                 }
+                return 0;
             })));
         });
         if (showStatus) {
@@ -102,19 +149,35 @@ public class PlayerctlControls implements ModInitializer {
                 @Override
                 public void run() {
                     try {
-                        String[] cmd = {"playerctl", "metadata", "-f", "'{{ artist }} - {{ title }}'"};
+                        String[] cmd = {"playerctl", "metadata", "-f", "'{{ mpris:artUrl }}'"};
                         Process proc = rt.exec(cmd);
                         BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
                         String s;
+                        String res = null;
                         while ((s = stdInput.readLine()) != null) {
-                            result = s;
+                            res = s.substring(1, s.length() - 1);
                         }
+                        if (res == null) {
+                            res = "https://listimg.pinclipart.com/picdir/s/330-3300806_red-slash-png-clipart-images-gallery-for-free.png";
+                            InputStream input = new URL(res).openStream();
+                            var nativeImage = NativeImage.read(input);
+                            nativeImageBackedTexture = new NativeImageBackedTexture(nativeImage);
+                        }
+                        if (!(Objects.equals(lastImg, res))) {
+                            InputStream input = new URL(res).openStream();
+                            var nativeImage = NativeImage.read(input);
+                            nativeImageBackedTexture = new NativeImageBackedTexture(nativeImage);
+                            lastImg = res;
+                        }
+                        cmd = new String[]{"playerctl", "metadata", "-f", "'{{ artist }} - {{ title }}'"};
+                        proc = rt.exec(cmd);
+                        stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+                        while ((s = stdInput.readLine()) != null) result = s.substring(1, s.length() - 1);
+                        if (result.equals("No players found")) result = "No players found";
                         cmd = new String[]{"playerctl", "status"};
                         proc = rt.exec(cmd);
                         stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
-                        while ((s = stdInput.readLine()) != null) {
-                            playing = s.equals("Playing");
-                        }
+                        while ((s = stdInput.readLine()) != null) playing = s.equals("Playing");
                     } catch (final IOException e) {
                         throw new RuntimeException(e);
                     }
--- src/main/java/net/marsupialgutz/pctl_meta/mixin/InGameHudMixin.java
+++ src/main/java/net/marsupialgutz/pctl_meta/mixin/InGameHudMixin.java
@@ -1,6 +1,8 @@
 package net.marsupialgutz.pctl_meta.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,11 +20,18 @@ public class InGameHudMixin {
     public void onRender(final MatrixStack matrices, final float tickDelta, final CallbackInfo info) {
         final var mc = MinecraftClient.getInstance();
         if (showStatus && !MinecraftClient.getInstance().options.debugEnabled) {
-            mc.textRenderer.drawWithShadow(matrices, "Song:", 5, 5, 0xffffff);
-            if (playing) {
-                mc.textRenderer.drawWithShadow(matrices, result.substring(1, result.length() - 1), 5, 15, 0xffffff);
-            } else {
-                mc.textRenderer.drawWithShadow(matrices, result.substring(1, result.length() - 1) + " (Paused)", 5, 15, 0xffffff);
+            var id = mc.getTextureManager().registerDynamicTexture("img", nativeImageBackedTexture);
+            if (!(result == null)) {
+                if (!(result.equals("No players found"))) {
+                    RenderSystem.setShaderTexture(0, id);
+                    DrawableHelper.drawTexture(matrices, 5, 5, 0, 0, 25, 25, 25, 25);
+                    mc.textRenderer.drawWithShadow(matrices, "Song:", 35, 8, 0xffffff);
+                    if (playing) {
+                        mc.textRenderer.drawWithShadow(matrices, result, 35, 18, 0xffffff);
+                    } else {
+                        mc.textRenderer.drawWithShadow(matrices, result + " (Paused)", 35, 18, 0xffffff);
+                    }
+                }
             }
         }
     }
